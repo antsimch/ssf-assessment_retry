82bcac81e3222ad5e1daac4ae3c1500a3bc7cf85
antsimch <anthonysimch@gmail.com>
2023-06-19 11:59:00 +0800

    task 1

--- src/main/java/vttp2023/batch3/ssf/frontcontroller/controllers/FrontController.java
+++ src/main/java/vttp2023/batch3/ssf/frontcontroller/controllers/FrontController.java
@@ -1,7 +1,23 @@
 package vttp2023.batch3.ssf.frontcontroller.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import vttp2023.batch3.ssf.frontcontroller.model.User;
+
+@Controller
 public class FrontController {
 
 	// TODO: Task 2, Task 3, Task 4, Task 6
-	
+	@GetMapping(path = "/")
+	public String getLandingPage() {
+		return "redirect:/login";
+	}
+
+	@GetMapping(path = "/login")
+	public String getLoginPage(Model model) {
+		model.addAttribute("user", new User());
+		return "view0";
+	}
 }
